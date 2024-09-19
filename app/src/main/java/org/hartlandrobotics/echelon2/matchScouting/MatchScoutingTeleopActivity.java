28f76b378b49578a8dae5f97cb502625b4d28a02
Dennis Burton <dennis.burton@gmail.com>
2024-09-19 14:13:12 -0400

    working teleop

--- app/src/main/java/org/hartlandrobotics/echelon2/matchScouting/MatchScoutingTeleopActivity.java
+++ app/src/main/java/org/hartlandrobotics/echelon2/matchScouting/MatchScoutingTeleopActivity.java
@@ -107,16 +107,16 @@ public class MatchScoutingTeleopActivity extends AppCompatActivity {
         midBasketText.setText(String.valueOf(matchResult.getTeleOpInt2()));
         lowBasketText.setText(String.valueOf(matchResult.getTeleOpInt3()));
 
-        topSpecimenText.setText(String.valueOf(matchResult.getAutoInt9()));
-        topSpecimenText.setText(String.valueOf(matchResult.getAutoInt10()));
+        topSpecimenText.setText(String.valueOf(matchResult.getTeleOpInt4()));
+        lowSpecimenText.setText(String.valueOf(matchResult.getTeleOpInt5()));
 
-        if( matchResult.getAutoFlag1() ){
+        if( matchResult.getEndFlag1() ){
             observationZoneButton.setImageResource(R.drawable.observation_zone_green);
         } else {
             observationZoneButton.setImageResource(observationZoneDrawable);
         }
 
-        defensesText.setText(String.valueOf(matchResult.getDefenseCount()));
+        //defensesText.setText(String.valueOf(matchResult.getDefenseCount()));
 
         if (matchResult.getEndFlag2()){
             level1AscentButton.setBackgroundTintList(ColorStateList.valueOf(getResources().getColor(R.color.secondaryDarkColor)));
@@ -157,7 +157,7 @@ public class MatchScoutingTeleopActivity extends AppCompatActivity {
         topBasketButton = findViewById(R.id.topBasket);
         topBasketButton.setImageResource(basketDrawable);
         topBasketButton.setOnClickListener(v -> {
-            matchResult.setAutoInt6( matchResult.getAutoInt6());
+            matchResult.setTeleOpInt1( matchResult.getTeleOpInt1() + 1 );
             populateControlsFromData();
         });
 
@@ -165,7 +165,7 @@ public class MatchScoutingTeleopActivity extends AppCompatActivity {
         midBasketButton = findViewById(R.id.midBasket);
         midBasketButton.setImageResource(basketDrawable);
         midBasketButton.setOnClickListener(v -> {
-            matchResult.setAutoInt7( matchResult.getAutoInt7());
+            matchResult.setTeleOpInt2( matchResult.getTeleOpInt2() + 1 );
             populateControlsFromData();
         });
 
@@ -173,40 +173,40 @@ public class MatchScoutingTeleopActivity extends AppCompatActivity {
         lowBasketButton = findViewById(R.id.lowBasket);
         lowBasketButton.setImageResource(basketDrawable);
         lowBasketButton.setOnClickListener(v -> {
-            matchResult.setAutoInt8( matchResult.getAutoInt8());
+            matchResult.setTeleOpInt3( matchResult.getTeleOpInt3() + 1);
             populateControlsFromData();
         });
 
         topSpecimenText = findViewById(R.id.topSpecimenText);
         topSpecimenButton = findViewById(R.id.topSpecimen);
-        topSpecimenButton.setImageResource(basketDrawable);
+        topSpecimenButton.setImageResource(specimemDrawable);
         topSpecimenButton.setOnClickListener(v -> {
-            matchResult.setAutoInt9( matchResult.getAutoInt9());
+            matchResult.setTeleOpInt4( matchResult.getTeleOpInt4() + 1);
             populateControlsFromData();
         });
 
         lowSpecimenText = findViewById(R.id.lowSpecimenText);
         lowSpecimenButton = findViewById(R.id.lowSpecimen);
-        lowSpecimenButton.setImageResource(basketDrawable);
+        lowSpecimenButton.setImageResource(specimemDrawable);
         lowSpecimenButton.setOnClickListener(v -> {
-            matchResult.setAutoInt9( matchResult.getAutoInt9());
+            matchResult.setTeleOpInt5( matchResult.getTeleOpInt5() + 1);
             populateControlsFromData();
         });
 
         observationZoneButton = findViewById(R.id.observationZone);
         observationZoneButton.setImageResource(observationZoneDrawable);
         observationZoneButton.setOnClickListener(v -> {
-            matchResult.setAutoFlag1( !matchResult.getAutoFlag1() );
+            matchResult.setEndFlag1( !matchResult.getEndFlag1() );
             populateControlsFromData();
         });
 
         level1AscentButton = findViewById(R.id.level1Ascent);
         level1AscentButton.setOnClickListener(v -> {
-            matchResult.setAutoFlag2(!matchResult.getAutoFlag2());
-            boolean isSelected = matchResult.getAutoFlag2();
+            matchResult.setEndFlag2(!matchResult.getEndFlag2());
+            boolean isSelected = matchResult.getEndFlag2();
             if ( isSelected ){
-                matchResult.setAutoFlag3(false);
-                matchResult.setAutoFlag4(false);
+                matchResult.setEndFlag3(false);
+                matchResult.setEndFlag4(false);
             }
 
             populateControlsFromData();
@@ -214,11 +214,11 @@ public class MatchScoutingTeleopActivity extends AppCompatActivity {
 
         level2AscentButton = findViewById(R.id.level2Ascent);
         level2AscentButton.setOnClickListener(v -> {
-            matchResult.setAutoFlag3(!matchResult.getAutoFlag3());
-            boolean isSelected = matchResult.getAutoFlag3();
+            matchResult.setEndFlag3(!matchResult.getEndFlag3());
+            boolean isSelected = matchResult.getEndFlag3();
             if ( isSelected ){
-                matchResult.setAutoFlag2(false);
-                matchResult.setAutoFlag4(false);
+                matchResult.setEndFlag2(false);
+                matchResult.setEndFlag4(false);
             }
 
             populateControlsFromData();
@@ -226,11 +226,11 @@ public class MatchScoutingTeleopActivity extends AppCompatActivity {
 
         level3AscentButton = findViewById(R.id.level3Ascent);
         level3AscentButton.setOnClickListener(v -> {
-            matchResult.setAutoFlag3(!matchResult.getAutoFlag4());
-            boolean isSelected = matchResult.getAutoFlag3();
+            matchResult.setEndFlag4(!matchResult.getEndFlag4());
+            boolean isSelected = matchResult.getEndFlag3();
             if ( isSelected ){
-                matchResult.setAutoFlag2(false);
-                matchResult.setAutoFlag3(false);
+                matchResult.setEndFlag2(false);
+                matchResult.setEndFlag3(false);
             }
 
             populateControlsFromData();
