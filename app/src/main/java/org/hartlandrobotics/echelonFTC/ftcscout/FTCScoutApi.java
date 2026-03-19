0a8195c98936d711f7b8cbf3f3a6fdcd3c652d8f
Dennis Burton <dennis.burton@gmail.com>
2026-03-19 18:45:52 -0400

    updates to ftcscout api

--- app/src/main/java/org/hartlandrobotics/echelonFTC/ftcscout/FTCScoutApi.java
+++ app/src/main/java/org/hartlandrobotics/echelonFTC/ftcscout/FTCScoutApi.java
@@ -1,4 +1,46 @@
 package org.hartlandrobotics.echelonFTC.ftcscout;
 
+import android.content.Context;
+import android.util.Log;
+
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import retrofit2.Retrofit;
+import retrofit2.converter.jackson.JacksonConverterFactory;
+
 public class FTCScoutApi {
+    static String TAG = "FTCScoutApi";
+
+    private static FTCScoutInterface ftcScoutInterface;
+
+    public static FTCScoutInterface getFtcScoutClient(Context context) {
+        if( ftcScoutInterface == null){
+            OkHttpClient.Builder httpClient = new OkHttpClient.Builder();
+            httpClient.addInterceptor(chain -> {
+                Request original = chain.request();
+                String path = chain.request().url().uri().getPath();
+                Log.e(TAG,path);
+                Request request = original.newBuilder()
+                        .header("Accept", "application/json")
+                        .build();
+                return chain.proceed(request);
+            });
+
+            OkHttpClient client = httpClient.build();
+            ObjectMapper mapper = new ObjectMapper();
+            mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+            Retrofit retrofit = new Retrofit.Builder()
+                    .baseUrl("https://api.ftcscout.org/rest/v1/")
+                    .addConverterFactory(JacksonConverterFactory.create(mapper))
+                    .client(client)
+                    .build();
+
+            ftcScoutInterface = retrofit.create(FTCScoutInterface.class);
+        }
+
+        return ftcScoutInterface;
+    }
 }
